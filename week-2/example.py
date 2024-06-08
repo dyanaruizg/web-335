19e76aef71e8d7c5064125625acb6a794988e69a
Diana Ruiz Garcia <dyanar_04@hotmail.com>
2024-06-07 20:24:01 -0700

    Add Hands-On 1.1: Building My First Python Program

---
+++ week-2/example.py
@@ -0,0 +1,15 @@
+"""
+Author: Diana Ruiz Garcia
+Date: 6/6/2024
+File Name: example.py
+Description: This file demonstrates the use of both file header comments and
+normal level comments
+"""
+
+# Importing required module (normal level comment explaining the following code block)
+import os
+
+# Getting the current working directory
+current_dir = os.getcwd() # normal level comment explaining this line of code
+
+print(current_dir) # Printing the current working directory
