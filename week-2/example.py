05e3a419f6c5ee36b6871ea19161600a2f33bbcd
Diana Ruiz Garcia <dyanar_04@hotmail.com>
2024-06-11 18:00:54 -0700

    Update on My First Python Program

--- week-2/example.py
+++ week-2/example.py
@@ -2,14 +2,7 @@
 Author: Diana Ruiz Garcia
 Date: 6/6/2024
 File Name: example.py
-Description: This file demonstrates the use of both file header comments and
-normal level comments
+Description: Building a simple Python program that prints a multiplication result.
 """
 
-# Importing required module (normal level comment explaining the following code block)
-import os
-
-# Getting the current working directory
-current_dir = os.getcwd() # normal level comment explaining this line of code
-
-print(current_dir) # Printing the current working directory
+print('3 * 4 = %d' % (3*4))
