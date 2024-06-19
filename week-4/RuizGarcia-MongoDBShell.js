62077761524e97989ad9926cfefaf9004323078c
Diana Ruiz Garcia <dyanar_04@hotmail.com>
2024-06-18 18:48:19 -0700

    Add Hands-On 4.2: MongoDB Database Setup and Querying with MongoDB Shell

---
+++ week-4/RuizGarcia-MongoDBShell.js
@@ -0,0 +1,21 @@
+/**
+	Title: RuizGarcia-MongoDBShell.js
+    Author: Diana Ruiz Garcia
+    Date: 18 June 2024
+    Description: Queries I wrote in hands-on project 4.2 using the MongoDB Shell.
+ */
+
+// Query that displays all users in the users collection.
+db.users.find();
+
+// Query that displays the user with the email address jbach@me.com.
+db.users.findOne({email:'jbach@me.com'});
+
+// Query that displays the user with the last name Mozart.
+db.users.findOne({lastName: 'Mozart'});
+
+// Query that displays the user with the first name Richard.
+db.users.findOne({firstName: 'Richard'});
+
+// Query that displays the user with employeeId 1010. 
+db.users.findOne({employeeId: '1010'});
